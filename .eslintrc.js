fef144b3dd51ba1a8a0e70222bbcdae0ae0e0909
Sarthak <7lovesharma7@gmail.com>
2021-02-22 22:23:27 +0530

    Adding ESLint and Prettier config 💯🚀

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react', 'prettier']
+};
